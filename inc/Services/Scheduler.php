5b555b66ea8d5228fc6dbf0723fdac6e2013556d
badasswp <badasswpdev@gmail.com>
2024-11-09 17:10:24 +0100

    chore: handle gracefull fallback

--- inc/Services/Scheduler.php
+++ inc/Services/Scheduler.php
@@ -106,7 +106,14 @@ class Scheduler extends Service implements Kernel {
 		$twilio = apply_filters( 'pbot_text_client', $this->get_twilio_client() );
 
 		foreach ( $this->get_pending_orders() as $order ) {
-			$twilio->send( $from, $order->get_billing_phone(), $message );
+			try {
+				$twilio->send( $from, $order->get_billing_phone(), $message );
+			} catch( \Exception $e ) {
+				error_log(
+					'Unable to send text message: %s',
+					$e->getMessage()
+				);
+			}
 		}
 	}
 
