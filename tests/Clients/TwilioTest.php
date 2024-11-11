20e7cacceff6569f31047f6efaafaf74148bd522
badasswp <badasswpdev@gmail.com>
2024-11-11 07:26:39 +0100

    test: add tests for Twilio

--- tests/Clients/TwilioTest.php
+++ tests/Clients/TwilioTest.php
@@ -6,6 +6,7 @@ use Mockery;
 use WP_Mock\Tools\TestCase;
 use PendingOrderBot\Clients\Twilio;
 use Twilio\Rest\Client as TwilioClient;
+use Twilio\Rest\Api\V2010\Account\MessageList as TwilioMessages;
 
 /**
  * @covers \PendingOrderBot\Clients\Twilio::__construct
@@ -26,5 +27,37 @@ class TwilioTest extends TestCase {
 
 	public function test_client_is_instance_of_twilio() {
 		$this->assertInstanceOf( TwilioClient::class, $this->twilio->get_client() );
+	}
+
+	public function test_client_sends_text_message() {
+		$messages = Mockery::mock( TwilioMessages::class )->makePartial();
+		$messages->shouldAllowMockingProtectedMethods();
+
+		$client = Mockery::mock( TwilioClient::class )->makePartial();
+		$client->shouldAllowMockingProtectedMethods();
+		$client->messages = $messages;
+
+		$twilio = Mockery::mock( Twilio::class )->makePartial();
+		$twilio->shouldAllowMockingProtectedMethods();
+		$twilio->client = $client;
+
+		$messages->shouldReceive( 'create' )
+			->with(
+				'+1(234)567890',
+				[
+					'from' => 'john@doe.com',
+					'body' => 'You have abandoned cart orders.',
+				]
+			);
+
+		$twilio->client->messages->create(
+			'+1(234)567890',
+			[
+				'from' => 'john@doe.com',
+				'body' => 'You have abandoned cart orders.',
+			]
+		);
+
+		$this->assertConditionsMet();
 	}
 }
