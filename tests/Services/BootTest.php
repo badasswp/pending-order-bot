832cc819ab38a49232699496cb3b1e9aa0d87e1f
badasswp <badasswpdev@gmail.com>
2024-11-11 06:43:10 +0100

    test: add tests for Boot class

---
+++ tests/Services/BootTest.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace PendingOrderBot\Tests\Services;
+
+use Mockery;
+use WP_Mock\Tools\TestCase;
+use PendingOrderBot\Services\Boot;
+use PendingOrderBot\Abstracts\Service;
+
+/**
+ * @covers \PendingOrderBot\Services\Boot::__construct
+ * @covers \PendingOrderBot\Services\Boot::register
+ * @covers \PendingOrderBot\Services\Boot::register_translation
+ */
+class BootTest extends TestCase {
+	public Boot $boot;
+
+	public function setUp(): void {
+		\WP_Mock::setUp();
+
+		$this->boot = new Boot();
+	}
+
+	public function tearDown(): void {
+		\WP_Mock::tearDown();
+	}
+
+	public function test_register() {
+		\WP_Mock::expectActionAdded( 'init', [ $this->boot, 'register_translation' ] );
+
+		$this->boot->register();
+
+		$this->assertConditionsMet();
+	}
+
+	public function test_register_translation() {
+		$boot = new \ReflectionClass( Boot::class );
+
+		\WP_Mock::userFunction( 'plugin_basename' )
+			->once()
+			->with( $boot->getFileName() )
+			->andReturn( '/inc/Services/Boot.php' );
+
+		\WP_Mock::userFunction( 'load_plugin_textdomain' )
+			->once()
+			->with(
+				'pending-order-bot',
+				false,
+				'/inc/Services/../../languages'
+			);
+
+		$this->boot->register_translation();
+
+		$this->assertConditionsMet();
+	}
+}
