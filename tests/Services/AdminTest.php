a876a73a50254b75762400dc7d9a3e347ec446c5
badasswp <badasswpdev@gmail.com>
2024-11-12 13:54:52 +0100

    test: add tests for Admin class

--- tests/Services/AdminTest.php
+++ tests/Services/AdminTest.php
@@ -4,11 +4,24 @@ namespace PendingOrderBot\Tests\Services;
 
 use Mockery;
 use WP_Mock\Tools\TestCase;
+use PendingOrderBot\Admin\Form;
+use PendingOrderBot\Admin\Options;
 use PendingOrderBot\Services\Admin;
 use PendingOrderBot\Abstracts\Service;
 
 /**
+ * @covers \PendingOrderBot\Services\Admin::__construct
  * @covers \PendingOrderBot\Services\Admin::register
+ * @covers \PendingOrderBot\Services\Admin::register_options_menu
+ * @covers \PendingOrderBot\Services\Admin::register_options_init
+ * @covers \PendingOrderBot\Services\Admin::register_options_styles
+ * @covers \PendingOrderBot\Engine\Watermarker::__construct
+ * @covers \PendingOrderBot\Admin\Options::__callStatic
+ * @covers \PendingOrderBot\Admin\Options::get_form_fields
+ * @covers \PendingOrderBot\Admin\Options::get_form_notice
+ * @covers \PendingOrderBot\Admin\Options::get_form_page
+ * @covers \PendingOrderBot\Admin\Options::get_form_submit
+ * @covers \PendingOrderBot\Admin\Options::init
  */
 class AdminTest extends TestCase {
 	public Admin $admin;
@@ -21,6 +34,8 @@ class AdminTest extends TestCase {
 
 	public function tearDown(): void {
 		\WP_Mock::tearDown();
+
+		$_POST = [];
 	}
 
 	public function test_register() {
@@ -29,6 +44,313 @@ class AdminTest extends TestCase {
 		\WP_Mock::expectActionAdded( 'admin_enqueue_scripts', [ $this->admin, 'register_options_styles' ] );
 
 		$this->admin->register();
+
+		$this->assertConditionsMet();
+	}
+
+	public function test_register_options_menu() {
+		\WP_Mock::userFunction(
+			'esc_html__',
+			[
+				'times'  => 57,
+				'return' => function ( $text, $domain = 'pending-order-bot' ) {
+					return $text;
+				},
+			]
+		);
+
+		\WP_Mock::userFunction(
+			'esc_attr',
+			[
+				'times'  => 30,
+				'return' => function ( $text ) {
+					return $text;
+				},
+			]
+		);
+
+		\WP_Mock::userFunction(
+			'esc_attr__',
+			[
+				'times'  => 0,
+				'return' => function ( $text ) {
+					return $text;
+				},
+			]
+		);
+
+		\WP_Mock::userFunction( 'add_menu_page' )
+			->with(
+				'Pending Order Bot',
+				'Pending Order Bot',
+				'manage_options',
+				'pending-order-bot',
+				[ $this->admin, 'register_options_page' ],
+				'dashicons-email-alt',
+				100
+			);
+
+		$this->admin->register_options_menu();
+
+		$this->assertConditionsMet();
+	}
+
+	public function test_register_options_init_bails_on_POST() {
+		\WP_Mock::userFunction(
+			'esc_html__',
+			[
+				'times'  => 57,
+				'return' => function ( $text, $domain = 'pending-order-bot' ) {
+					return $text;
+				},
+			]
+		);
+
+		\WP_Mock::userFunction(
+			'esc_attr',
+			[
+				'times'  => 30,
+				'return' => function ( $text ) {
+					return $text;
+				},
+			]
+		);
+
+		\WP_Mock::userFunction(
+			'esc_attr__',
+			[
+				'times'  => 0,
+				'return' => function ( $text ) {
+					return $text;
+				},
+			]
+		);
+
+		$this->admin->register_options_init();
+
+		$this->assertConditionsMet();
+	}
+
+	public function test_register_options_init_bails_on_NONCE() {
+		\WP_Mock::userFunction(
+			'esc_html__',
+			[
+				'times'  => 57,
+				'return' => function ( $text, $domain = 'pending-order-bot' ) {
+					return $text;
+				},
+			]
+		);
+
+		\WP_Mock::userFunction(
+			'esc_attr',
+			[
+				'times'  => 30,
+				'return' => function ( $text ) {
+					return $text;
+				},
+			]
+		);
+
+		\WP_Mock::userFunction(
+			'esc_attr__',
+			[
+				'times'  => 0,
+				'return' => function ( $text ) {
+					return $text;
+				},
+			]
+		);
+
+		$_POST = [
+			'pending_order_bot_save_settings'  => true,
+			'pending_order_bot_settings_nonce' => 'a8jfkgw2h7i',
+		];
+
+		\WP_Mock::userFunction( 'wp_unslash' )
+			->with( 'a8jfkgw2h7i' )
+			->andReturn( 'a8jfkgw2h7i' );
+
+		\WP_Mock::userFunction( 'sanitize_text_field' )
+			->with( 'a8jfkgw2h7i' )
+			->andReturn( 'a8jfkgw2h7i' );
+
+		\WP_Mock::userFunction( 'wp_verify_nonce' )
+			->with( 'a8jfkgw2h7i', 'pending_order_bot_settings_action' )
+			->andReturn( false );
+
+		$this->admin->register_options_init();
+
+		$this->assertConditionsMet();
+	}
+
+	public function test_register_options_init_passes() {
+		\WP_Mock::userFunction(
+			'esc_html__',
+			[
+				'times'  => 95,
+				'return' => function ( $text, $domain = 'pending-order-bot' ) {
+					return $text;
+				},
+			]
+		);
+
+		\WP_Mock::userFunction(
+			'esc_attr',
+			[
+				'times'  => 50,
+				'return' => function ( $text ) {
+					return $text;
+				},
+			]
+		);
+
+		\WP_Mock::userFunction(
+			'esc_attr__',
+			[
+				'times'  => 0,
+				'return' => function ( $text ) {
+					return $text;
+				},
+			]
+		);
+
+		$_POST = [
+			'pending_order_bot_save_settings'  => true,
+			'pending_order_bot_settings_nonce' => 'a8jfkgw2h7i',
+		];
+
+		\WP_Mock::userFunction( 'wp_verify_nonce' )
+			->once()
+			->with( 'a8jfkgw2h7i', 'pending_order_bot_settings_action' )
+			->andReturn( true );
+
+		\WP_Mock::userFunction(
+			'wp_unslash',
+			[
+				'times'  => 7,
+				'return' => function ( $text ) {
+					return $text;
+				},
+			]
+		);
+
+		\WP_Mock::userFunction(
+			'sanitize_text_field',
+			[
+				'times'  => 7,
+				'return' => function ( $text ) {
+					return $text;
+				},
+			]
+		);
+
+		\WP_Mock::userFunction( 'update_option' )
+			->once()
+			->with(
+				'pending_order_bot',
+				[
+					'twilio_sid'     => '',
+					'twilio_token'   => '',
+					'twilio_phone'   => '',
+					'twilio_message' => '',
+					'send_text'      => '',
+					'send_email'     => '',
+				]
+			)
+			->andReturn( true );
+
+		$this->admin->register_options_init();
+
+		$this->assertConditionsMet();
+	}
+
+	public function test_register_options_init_updates_POST_values_that_are_set() {
+		\WP_Mock::userFunction(
+			'esc_html__',
+			[
+				'times'  => 95,
+				'return' => function ( $text, $domain = 'pending-order-bot' ) {
+					return $text;
+				},
+			]
+		);
+
+		\WP_Mock::userFunction(
+			'esc_attr',
+			[
+				'times'  => 50,
+				'return' => function ( $text ) {
+					return $text;
+				},
+			]
+		);
+
+		\WP_Mock::userFunction(
+			'esc_attr__',
+			[
+				'times'  => 0,
+				'return' => function ( $text ) {
+					return $text;
+				},
+			]
+		);
+
+		$updated_options = [
+			'size'   => 60,
+			'label'  => 'WATERMARK',
+			'upload' => true,
+		];
+
+		$_POST = array_merge(
+			$updated_options,
+			[
+				'pending_order_bot_save_settings'  => true,
+				'pending_order_bot_settings_nonce' => 'a8jfkgw2h7i',
+			]
+		);
+
+		\WP_Mock::userFunction( 'wp_verify_nonce' )
+			->once()
+			->with( 'a8jfkgw2h7i', 'pending_order_bot_settings_action' )
+			->andReturn( true );
+
+		\WP_Mock::userFunction(
+			'wp_unslash',
+			[
+				'times'  => 7,
+				'return' => function ( $text ) {
+					return $text;
+				},
+			]
+		);
+
+		\WP_Mock::userFunction(
+			'sanitize_text_field',
+			[
+				'times'  => 7,
+				'return' => function ( $text ) {
+					return $text;
+				},
+			]
+		);
+
+		\WP_Mock::userFunction( 'update_option' )
+			->once()
+			->with(
+				'pending_order_bot',
+				[
+					'twilio_sid'     => '',
+					'twilio_token'   => '',
+					'twilio_phone'   => '',
+					'twilio_message' => '',
+					'send_text'      => '',
+					'send_email'     => '',
+				]
+			)
+			->andReturn( true );
+
+		$this->admin->register_options_init();
 
 		$this->assertConditionsMet();
 	}
