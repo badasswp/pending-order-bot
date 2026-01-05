73375ef03a9ff6842ed0b4917fb97345d7828dac
badasswp <badasswpdev@gmail.com>
2026-01-05 04:51:55 +0000

    push to master-built

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'badasswp/pending-order-bot',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => 'c61979fa6182197254c52038d7efc20b95931d35',
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'badasswp/pending-order-bot' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => 'c61979fa6182197254c52038d7efc20b95931d35',
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'twilio/sdk' => array(
+            'pretty_version' => '8.3.9',
+            'version' => '8.3.9.0',
+            'reference' => 'b8a79247ad00daed6f823328a052293d498fac36',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../twilio/sdk',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
